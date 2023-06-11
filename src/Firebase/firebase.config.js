c926801eac27faac3827f72c3c66e8fb322d0aad
sajibkhansk <sajibkhan117593@gmial.com>
2023-06-12 02:16:31 +0600

    key hide

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,18 +1,17 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
 
-// Your web app's Firebase configuration
+import 'firebase/firestore';
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBCSf81rnzGnpWfKJ8zzguFInXLqz1XvlY",
-  authDomain: "sk-sporting-club.firebaseapp.com",
-  projectId: "sk-sporting-club",
-  storageBucket: "sk-sporting-club.appspot.com",
-  messagingSenderId: "957231411918",
-  appId: "1:957231411918:web:4cbb17915b439c69a5169f"
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export default app;
